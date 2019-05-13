ea6ab914cce3c4976c20f8e733217e699c9815c6
romain <romain.legal44@gmail.com>
2019-05-13 17:03:04 +0200

    create method show

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -16,5 +17,15 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
             'owner' => 'Thomas',
         ]);
+    }
+    /**
+     * @Route("/blog/show/{slug?Article Sans Titre}", name="blog_show",
+     *  requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"})
+     */
+    public function show(string $slug): Response
+    {
+       $slugg = str_replace("-", " ",$slug);
+        $newSlug = ucfirst($slugg);
+        return $this->render('show.html.twig',['slug' => $newSlug]);
     }
 }
