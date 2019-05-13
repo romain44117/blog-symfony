f8df64ea8a88fd5690dc6d3cb738070ced7fa0c9
romain <romain.legal44@gmail.com>
2019-05-13 12:02:29 +0200

    add blog controller and default controller

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog_index")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Thomas',
+        ]);
+    }
+}
